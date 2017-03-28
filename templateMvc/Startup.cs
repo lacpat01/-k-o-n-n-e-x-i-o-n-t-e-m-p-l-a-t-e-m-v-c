6b7689d8e9bc9d66c3d8f2b2c93a256d96b03832
lacpat01 <patrickla@hotmail.com>
2017-03-28 14:13:42 -0400

    Ajout du projet

---
+++ templateMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(templateMvc.Startup))]
+namespace templateMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
